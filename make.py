38771ecb665d69eef013d204dd8e2ac7fd7a5c97
Pierre Raybaut <pierre.raybaut@gmail.com>
2012-08-17 17:10:45 +0200

    make: now making the launchers automatically

--- make.py
+++ make.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright © 2012 Pierre Raybaut
+# Licensed under the terms of the MIT License
+# (see winpython/__init__.py for details)
+
 """
 WinPython build script
 
 Created on Sun Aug 12 11:17:50 2012
 """
 
+from __future__ import print_function
+
 import os
 import os.path as osp
 import re
 import subprocess
 import shutil
+import sys
 
 from guidata import disthelpers
 
@@ -29,7 +37,7 @@ def bat_to_exe(fname, icon):
     bname = osp.basename(fname)
     args = ['-bat', bname, '-save', bname.replace('.bat', '.exe')]#,
 #            '-icon', osp.abspath(icon)]
-    print args
+    print(args)
     subprocess.call([conv]+args, cwd=osp.dirname(fname))
 
 def call_bat_from_exe(batname, targetname, icon):
@@ -53,18 +61,79 @@ def bat_to_exe_test():
     call_bat_from_exe(osp.join(wpdir, 'scripts', 'spyder.bat'), 'Spyder.exe',
                       osp.join(wpdir, 'python-2.7.3.amd64',
                                'Scripts', 'spyder.ico'))
-        
+
+
+def get_nsis_exe():
+    """Return NSIS executable"""
+    drive = __file__[:3]
+    for dirname in (r'C:\Program Files', r'C:\Program Files (x86)',
+                    drive+r'PortableApps\NSISPortableANSI',
+                    drive+r'PortableApps\NSISPortable',
+                    drive+r'PortableApps\PortableApps\NSISPortableANSI',
+                    drive+r'PortableApps\PortableApps\NSISPortable',
+                ):
+        for subdirname in ('.', 'App'):
+            exe = osp.join(dirname, subdirname, 'NSIS', 'makensis.exe')
+            if osp.isfile(exe):
+                return exe
+    else:
+        raise RuntimeError, "NSIS is not installed on this computer."
+
+NSIS_EXE = get_nsis_exe()
+
+
+def replace_in_nsis_file(fname, data):
+    """Replace text in line starting with *start*, from this position:
+    data is a list of (start, text) tuples"""
+    fd = open(fname, 'U')
+    lines = fd.readlines()
+    fd.close()
+    for idx, line in enumerate(lines):
+        for start, text in data:
+            if start not in ('Icon', 'OutFile'):
+                start = '!define ' + start
+            if line.startswith(start):
+                lines[idx] = line[:len(start)+1] + ('"%s"' % text) + '\n'
+    fd = open(fname, 'w')
+    fd.writelines(lines)
+    fd.close()
+
 
 class WinPythonDistribution(object):
     """WinPython distribution"""
-    def __init__(self, target, instdir):
+    def __init__(self, target, instdir, verbose=False):
         self.target = target
         self.instdir = instdir
+        self.verbose = verbose
         self.version = None
         self.fullversion = None
         self.winpydir = None
+        self.python_name = None
         self.distribution = None
         self.installed_packages = []
+
+    @property
+    def ms_arch(self):
+        """Return distribution architecture, in Microsoft format: x86/x64"""
+        return 'x64' if 'amd64' in self.python_name else 'x86'
+        
+    @property
+    def py_arch(self):
+        """Return distribution architecture, in Python distutils format:
+        win-amd64 or win32"""
+        return 'win-amd64' if 'amd64' in self.python_name else 'win32'
+    
+    @property
+    def prepath(self):
+        """Return PATH contents to be prepend to the environment variable"""
+        return [r"Lib\site-packages\PyQt4"]
+    
+    @property
+    def postpath(self):
+        """Return PATH contents to be append to the environment variable"""
+        return ["", "DLLs", "Scripts",
+                r"..\tools", r"..\tools\gnuwin32\bin",
+                r"..\tools\TortoiseHg-%s" % self.ms_arch]
         
     def get_package_fname(self, pattern):
         """Get package matching pattern in instdir"""
@@ -86,41 +155,98 @@ class WinPythonDistribution(object):
 
     def create_batch_script(self, name, contents):
         """Create batch script %WINPYDIR%/name"""
-        assert name.endswith('.bat')
         scriptdir = osp.join(self.winpydir, 'scripts')
         if not osp.isdir(scriptdir):
             os.mkdir(scriptdir)
         fd = file(osp.join(scriptdir, name), 'w')
         fd.write(contents)
         fd.close()
+    
+    def create_launcher(self, name, icon,
+                        command=None, args=None, workdir=None):
+        """Create exe launcher with NSIS"""
+        assert name.endswith('.exe')
+        portable_dir = osp.join(osp.dirname(__file__), 'portable')
+        icon_fname = osp.join(portable_dir, 'icons', icon)
+        assert osp.isfile(icon_fname)
+        fname = osp.join(self.winpydir, osp.splitext(name)[0]+'.nsi')
+        shutil.copy(osp.join(portable_dir, 'launcher.nsi'), fname)
+        
+        # Customizing NSIS script
+        conv = lambda path: ";".join(['${WINPYDIR}\\'+pth for pth in path])
+        prepath = conv(self.prepath)
+        postpath = conv(self.postpath)
+        if command is None:
+            if args is not None and '.pyw' in args:
+                command = '${WINPYDIR}\pythonw.exe'
+            else:
+                command = '${WINPYDIR}\python.exe'
+        if args is None:
+            args = ''
+        if workdir is None:
+            workdir = ''
+        replace_in_nsis_file(fname,
+                        (('WINPYDIR', '$EXEDIR\%s' % self.python_name),
+                         ('COMMAND', command),
+                         ('PARAMETERS', args),
+                         ('WORKDIR', workdir),
+                         ('PREPATH', prepath),
+                         ('POSTPATH', postpath),
+                         ('Icon', icon_fname),
+                         ('OutFile', name),
+                        ))
+        
+        try:
+            retcode = subprocess.call('"%s" -V2 %s' % (NSIS_EXE, fname),
+                                      shell=True)
+            if retcode < 0:
+                print >>sys.stderr, "Child was terminated by signal", -retcode
+        except OSError, e:
+            print >>sys.stderr, "Execution failed:", e
+        os.remove(fname)
+
+    def _print(self, text):
+        """Print action text indicating progress"""
+        if self.verbose:
+            utils.print_box(text)
+        else:
+            print(text + '...', end=" ")
+    
+    def _print_done(self):
+        """Print OK at the end of a process"""
+        if not self.verbose:
+            print("OK")
 
     def make(self):
         """Make WinPython distribution in target directory from the installers 
         located in instdir"""
         python_fname = self.get_package_fname(
                                     r'python-([0-9\.]*)(\.amd64)?\.msi')
-        python_name = osp.basename(python_fname)[:-4]
+        self.python_name = python_name = osp.basename(python_fname)[:-4]
         distname = 'win%s' % python_name
         vlst = re.match(r'winpython-([0-9\.]*)', distname
                         ).groups()[0].split('.')
         self.version = '.'.join(vlst[:2])
         self.fullversion = '.'.join(vlst[:3])
-        
+
         # Create the WinPython base directory
-        utils.print_box("Creating WinPython base directory")
+        self._print("Creating WinPython base directory")
         self.winpydir = osp.join(self.target, distname)
         if osp.isdir(self.winpydir):
             shutil.rmtree(self.winpydir)
         os.mkdir(self.winpydir)
+        self._print_done()
 
         # Extracting Python installer, creating distribution object
-        utils.print_box("Extracting Python installer")
-        wppm.extract_msi(python_fname, targetdir=self.winpydir)
+        self._print("Extracting Python installer")
+        utils.extract_msi(python_fname, targetdir=self.winpydir)
         self.installed_packages.append(python_fname)
         pydir = osp.join(self.winpydir, python_name)
         os.remove(osp.join(pydir, osp.basename(python_fname)))
-        self.distribution = wppm.Distribution(pydir)
+        self.distribution = wppm.Distribution(pydir, verbose=self.verbose,
+                                              indent=True)
         os.mkdir(osp.join(pydir, 'Scripts'))
+        self._print_done()
         
         # Adding Microsoft Visual Studio 2008 DLLs and manifest
         print("Adding Microsoft Visual C++ 2008 DLLs with manifest""")
@@ -129,64 +255,90 @@ class WinPythonDistribution(object):
                                 python_version=self.distribution.version):
             shutil.copy(fname, pydir)
 
-        arch2 = 'win-amd64' if 'amd64' in distname else 'win32'
-
-        # Install pywin32
+        # Installing required packages
+        print("Installing required packages")
         self.install_package('pywin32-([0-9\.]*[a-z]*).%s-py%s.exe'
-                             % (arch2, self.version))
-
-        # Install winpython package (wppm)
+                             % (self.py_arch, self.version))
         self.install_package('winpython-([0-9\.]*[a-z]*).%s(-py%s)?.exe'
-                             % (arch2, self.version))
-
-        # Install spyderlib package (Spyder)
+                             % (self.py_arch, self.version))
         self.install_package('spyder(lib)?-([0-9\.]*[a-z]*).%s(-py%s)?.exe'
-                             % (arch2, self.version))
-        
-        # Install PyQt and PyQwt
-        arch1 = 'x64' if 'amd64' in distname else 'x86'
+                             % (self.py_arch, self.version))
         self.install_package(pattern='PyQt-Py%s-%s-gpl-([0-9\.\-]*).exe'
-                                     % (self.version, arch1))
-        file(osp.join(pydir, 'qt.conf'), 'w').write("""[Paths]
-Prefix = ./Lib/site-packages/PyQt4
-Binaries = ./Lib/site-packages/PyQt4""")
-        file(osp.join(pydir, 'Lib', 'site-packages', 'PyQt4', 'qt.conf'),
-             'w').write("""[Paths]
-Prefix = .
-Binaries = .""")
+                                     % (self.version, self.ms_arch))
         self.install_package(
                     pattern='PyQwt-([0-9\.]*)-py%s-%s-([a-z0-9\.\-]*).exe'
-                            % (self.version, arch1))
+                            % (self.version, self.ms_arch))
         
         # Try to install all other packages in instdir
+        print("Installing other packages")
         for fname in os.listdir(self.instdir):
             try:
                 self.install_package(fname)
             except NotImplementedError:
                 pass
         
-        # Show stats
-        utils.print_box("Installed packages")
-        for fname in self.installed_packages:
-            print "   ", fname
-        
         # Copy dev tools
-        utils.print_box("Copying tools")
+        self._print("Copying tools")
         shutil.copytree(osp.join(osp.dirname(__file__), 'tools'),
                         osp.join(self.winpydir, 'tools'))
+        self._print_done()
+        
+        #XXX: the USERPROFILE environment variable redirection do not work 
+        # with TortoiseHg (and maybe other programs)... but, why?
+        ## Creating settings directory
+        #os.mkdir(osp.join(self.winpydir, 'settings'))
+
+        # Create launchers
+        self._print("Creating launchers")
+        self.create_launcher('cmd.exe', 'cmd.ico', command='$SYSDIR\cmd.exe',
+                             args='/k', workdir='${WINPYDIR}')
+        self.create_launcher('python.exe', 'python.ico')
+        self.create_launcher('Spyder.exe', 'spyder.ico',
+                             args='spyder',
+                             workdir='${WINPYDIR}\Scripts')
+        self.create_launcher('Spyder_Light.exe', 'spyder_light.ico',
+                             args='spyder --light',
+                             workdir='${WINPYDIR}\Scripts')
+        self.create_launcher('WPPM.exe', 'winpython.ico',
+                             args='wppmgui.pyw',
+                             workdir='${WINPYDIR}\Scripts')
+        self.create_launcher('PyQtdemo.exe', 'qt.ico', args='qtdemo.pyw',
+           workdir='${WINPYDIR}\Lib\site-packages\PyQt4\examples\demos\qtdemo')
+        thg = r'\tools\TortoiseHg-%s\thg.exe' % self.ms_arch
+        if osp.isfile(self.winpydir + thg):
+            self.create_launcher('TortoiseHg.exe', 'tortoisehg.ico',
+                                 command=r'${WINPYDIR}\..' + thg,
+                                 workdir=r'${WINPYDIR}')
+        winmerge = r'\tools\WinMerge\WinMerge.exe'
+        if osp.isfile(self.winpydir + winmerge):
+            self.create_launcher('WinMerge.exe', 'winmerge.ico',
+                                 command=r'${WINPYDIR}\..' + winmerge,
+                                 workdir=r'${WINPYDIR}')
+        self._print_done()
 
         # Create batch scripts
+        print("Creating batch scripts")
+        self.create_batch_script('readme.txt', \
+r"""These batch files are not required to run WinPython.
+
+The purpose of these files is to help the user writing his/her own 
+batch file to call Python scripts inside WinPython.
+The examples here ('spyder.bat', 'spyder_light.bat', 'wppm.bat', 
+'pyqt_demo.bat', 'python.bat' and 'cmd.bat') are quite similar to the 
+launchers located in the parent directory.
+The environment variables are set-up in 'env.bat'.""")
+        conv = lambda path: ";".join(['%WINPYDIR%\\'+pth for pth in path])
+        path = conv(self.prepath) + ";%PATH%;" + conv(self.postpath)
         self.create_batch_script('env.bat', """@echo off
 set WINPYDIR=%~dp0..\\""" + python_name + r"""
-set PATH=%WINPYDIR%\Lib\site-packages\PyQt4;%PATH%
-set PATH=%PATH%;%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts
-set PATH=%PATH%;%~dp0..\tools;%~dp0..\tools\gnuwin32\bin
-set PATH=%PATH%;%~dp0..\tools\TortoiseHg-"""+arch1)
-
+set PATH=""" + path)
+        #self.create_batch_script('env.bat', """@echo off
+#set WINPYDIR=%~dp0..\\""" + python_name + r"""
+#set USERPROFILE=%WINPYDIR%\..\settings
+#set PATH=""" + path)
         self.create_batch_script('cmd.bat', r"""@echo off
 call %~dp0env.bat
 cmd.exe /k""")
-
         self.create_python_batch('python.bat', '', '')
         self.create_python_batch('spyder.bat', r'\Scripts', 'spyder')
         self.create_python_batch('spyder_light.bat', r'\Scripts', 'spyder',
@@ -195,7 +347,9 @@ cmd.exe /k""")
         self.create_python_batch('pyqt_demo.bat',
              r'\Lib\site-packages\PyQt4\examples\demos\qtdemo', 'qtdemo.pyw')
 
+        self._print("Cleaning up distribution")
         self.distribution.clean_up()
+        self._print_done()
         
     def create_python_batch(self, name, package_dir, script_name,
                             options=None):
@@ -223,5 +377,5 @@ if __name__ == '__main__':
     instdir = osp.join(sbdir, 'installers')
     if not osp.isdir(wpdir):
         os.mkdir(wpdir)
-    dist = WinPythonDistribution(wpdir, instdir)
+    dist = WinPythonDistribution(wpdir, instdir, verbose=False)
     dist.make()
--- portable/launcher.nsi
+++ portable/launcher.nsi
@@ -7,6 +7,8 @@
 ; The following lines are automatically replaced when creating launchers:
 !define WINPYDIR "$EXEDIR\python-2.7.3.amd64"
 !define COMMAND "${WINPYDIR}\python.exe"
+!define PARAMETERS ""
+!define WORKDIR ""
 !define PREPATH "${WINPYDIR}\Lib\site-packages\PyQt4"
 !define POSTPATH "${WINPYDIR}\;${WINPYDIR}\DLLs;${WINPYDIR}\Scripts;${WINPYDIR}\..\tools;${WINPYDIR}\..\tools\gnuwin32\bin;${WINPYDIR}\..\tools\TortoiseHg-x64"
 Icon "icons\python.ico"
@@ -21,12 +23,26 @@ Call Execute
 SectionEnd
 
 Function Execute
+StrCmp ${WORKDIR} "" 0 workdir
 System::Call "kernel32::GetCurrentDirectory(i ${NSIS_MAX_STRLEN}, t .r0)"
 SetOutPath $0
+Goto endworkdir
+workdir:
+SetOutPath "${WORKDIR}"
+endworkdir:
+
 System::Call 'Kernel32::SetEnvironmentVariableA(t, t) i("WINPYDIR", "${WINPYDIR}").r0'
+; System::Call 'Kernel32::SetEnvironmentVariableA(t, t) i("USERPROFILE", "$EXEDIR\settings").r0'
 ReadEnvStr $R0 "PATH"
 StrCpy $R0 "${PREPATH};$R0;${POSTPATH}"
 System::Call 'Kernel32::SetEnvironmentVariableA(t, t) i("PATH", R0).r0'
+
+StrCmp "${PARAMETERS}" "" 0 param
 ${GetParameters} $R1
+Goto endparam
+param:
+StrCpy $R1 "${PARAMETERS}"
+endparam:
+
 Exec '"${COMMAND}" $R1'
 FunctionEnd
