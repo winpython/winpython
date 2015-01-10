f4d0e7d18ec29cbacc3e8a3c8990a34795c41d51
stonebig <stonebig..>
2015-01-10 19:30:39 +0100

    Version 1.1
    A complete tour of wheel.

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -27,6 +27,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '1.0'
+__version__ = '1.1'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
