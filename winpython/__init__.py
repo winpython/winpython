bfc18b74a2108b8bceb00174912a9c21db2a035f
stonebig <stonebig>
2016-08-16 20:00:33 +0200

    increment version + package names

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '1.6.20160625'
+__version__ = '1.6.20160816'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
