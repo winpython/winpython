9e0021e27aba0342d4bf522a43d375c2fa718160
stonebig <stonebig>
2016-09-22 19:38:19 +0200

    remove what seems now a bad idea

--- make.py
+++ make.py
@@ -980,7 +980,7 @@ cd/D "%WINPYWORKDIR%"
 call "%~dp0env.bat"
 echo this will upgrade pip with latest version, then patch it for WinPython portability ok ?
 pause
-"%WINPYDIR%\python.exe" -m pip install --upgrade --force-reinstall  pip
+"%WINPYDIR%\python.exe" -m pip install --upgrade pip
 "%WINPYDIR%\python.exe" -c "from winpython import wppm;dist=wppm.Distribution(r'%WINPYDIR%');dist.patch_standard_packages('pip', to_movable=True)
 pause
 """)
