938fa61ac2621317e571f8c2863ff0e4b981ebd1
stonebig <stonebig34@gmail.com>
2018-10-31 16:24:37 +0100

    switch from NSIS to INNO installer

---
+++ generate_winpython_distros37_qt5.bat
@@ -0,0 +1,43 @@
+rem  this replace running manually from spyder the make.py 
+rem  to launch from a winpython module 'make' directory 
+
+set my_original_path=%path%
+set my_buildenv=C:\winpython-64bit-3.4.3.7Qt5
+
+set my_root_dir_for_builds=C:\Winp
+set my_python_target=37
+set my_pyver=3.7
+
+set my_flavor=
+
+set my_release=0
+
+set my_release_level=
+
+set my_install_options=--no-index --pre --trusted-host=None
+set my_find_links=C:\Winp\packages.srcreq
+set my_docsdirs=C:\Winp\bd37\docs
+
+
+set tmp_reqdir=%my_root_dir_for_builds%\bd%my_python_target%
+
+set my_arch=32
+set my_requirements=C:\Winp\bd37\Qt5_requirements.txt
+set my_source_dirs=C:\Winp\bd37\packages.win32.Qt5
+set my_toolsdirs=C:\Winp\bd37\Tools
+
+set my_preclear_build_directory=Yes
+call %~dp0\generate_a_winpython_distro.bat
+
+set my_arch=64
+set my_requirements=C:\Winp\bd37\Qt5_requirements64.txt
+set my_source_dirs=C:\Winp\bd37\packages.win-amd64.Qt5
+set my_toolsdirs=C:\Winp\bd37\Tools64
+
+set my_preclear_build_directory=No
+
+
+call %~dp0\generate_a_winpython_distro.bat
+
+
+pause
