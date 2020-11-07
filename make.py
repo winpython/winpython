bca874a4b9f8655ea7a758321516fafca6d78dfc
stonebig <stonebig34@gmail.com>
2020-11-07 20:15:31 +0100

    resolve the ampersand character problem
    https://github.com/winpython/winpython/issues/899

--- make.py
+++ make.py
@@ -1353,7 +1353,7 @@ If WScript.Arguments.Count >= 1 Then
     ReDim arr(WScript.Arguments.Count-1)
     For i = 0 To WScript.Arguments.Count-1
         Arg = WScript.Arguments(i)
-        If InStr(Arg, " ") > 0 Then Arg = chr(34) & Arg & chr(34)
+        If InStr(Arg, " ") > 0 or InStr(Arg, "&") > 0 Then Arg = chr(34) & Arg & chr(34)
       arr(i) = Arg
     Next
 
