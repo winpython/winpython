3372d3ce76c2820b36f736c8278765cf410ee178
stonebig <stonebig34@gmail.com>
2021-08-12 08:59:54 +0200

    increment version

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '4.4.20210807'
+__version__ = '4.4.20210812'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
