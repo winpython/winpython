a363dab54a98261d31908e81016258082baeff75
stonebig <stonebig34@gmail.com>
2022-08-09 12:15:10 +0200

    tag the windows terminal change

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '4.7.20220807'
+__version__ = '4.8.20220808'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
