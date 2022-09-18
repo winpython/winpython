34a025e1ed631906e6b8f9f9efe5163c8c03a52e
stonebig <stonebig34@gmail.com>
2022-09-18 18:27:34 +0200

    update 2022-09-18

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '5.0.20220813'
+__version__ = '5.0.20220918'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
