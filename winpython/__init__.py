444e9bc4c334b680029bf1df5bb102bcff6ed02d
stonebig <stonebig34@gmail.com>
2023-08-26 16:58:33 +0200

    restore (better) Vscode in /t

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '7.0.20230822'
+__version__ = '7.0.20230826'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
