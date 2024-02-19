78c9fd47ba286524510c7b17a35bd795e5ad23f9
stonebig <stonebig34@gmail.com>
2024-02-19 18:50:07 +0100

    add wppm -lsa -v
    trying to be more practical than METADATA or pip inspect

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '7.2.20240218'
+__version__ = '7.2.20240219'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
