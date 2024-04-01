bc107826e2927cbd5e8a2f570177a9da48bf09c2
stonebig <stonebig34@gmail.com>
2024-04-01 15:06:38 +0200

    update names

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '7.5.20240317'
+__version__ = '7.5.20240401'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
