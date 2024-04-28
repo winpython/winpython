5ad2d1f50015597db5f4246f682f4d7c1053676f
stonebig <stonebig34@gmail.com>
2024-04-28 10:57:08 +0200

    pip search is gone
    decrepited per pip

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '7.5.20240410'
+__version__ = '7.6.20240427'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
