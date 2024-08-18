6b3d4128da622401975ead6a7aa96276d6978e24
stonebig <stonebig34@gmail.com>
2024-08-18 19:45:37 +0200

    include mkshim.py
    mkshim.py piece from https://github.com/pfmoore/shimmy

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '9.1.20240815'
+__version__ = '10.0.20240818'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
