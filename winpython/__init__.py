3c7a96f51eb150604b2e453ae8b2a4bd29459863
stonebig <stonebig34@gmail.com>
2024-09-01 16:43:42 +0200

    improved icon launcher generators

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '10.2.20240825'
+__version__ = '10.3.20240901'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
