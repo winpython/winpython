bd52bd9595a01b5316f878522f5b6e4b8db26cb6
stonebig <stonebig34@gmail.com>
2024-09-22 11:03:02 +0200

    remove all '.vbs'  from build system

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '11.0.20240915'
+__version__ = '11.1.20240922'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
