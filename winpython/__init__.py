9bfb1059dd24f2c4c8db3abf1275a0354e701ea4
stonebig <stonebig34@gmail.com>
2025-01-03 12:53:06 +0100

    post rc1fix

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '11.2.20241226'
+__version__ = '11.2.20241228'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
