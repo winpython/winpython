e1b0fbfe104e9dc4280f6ab33aee3647df0fdd84
stonebig <stonebig34@gmail.com>
2025-01-05 00:15:46 +0100

    remove last vbs code

--- make.py
+++ make.py
@@ -674,23 +674,6 @@ if not exist "%HOME%\.spyder-py%WINPYVER:~0,1%\workingdir" echo %HOME%\Notebooks
             do_changes=changes,
         )
 
-        self.create_batch_script(
-            "Noshell.vbs",
-            r"""
-'from http://superuser.com/questions/140047/how-to-run-a-batch-file-without-launching-a-command-window/390129
-If WScript.Arguments.Count >= 1 Then
-    ReDim arr(WScript.Arguments.Count-1)
-    For i = 0 To WScript.Arguments.Count-1
-        Arg = WScript.Arguments(i)
-        If InStr(Arg, " ") > 0 or InStr(Arg, "&") > 0 Then Arg = chr(34) & Arg & chr(34)
-      arr(i) = Arg
-    Next
-
-    RunCmd = Join(arr)
-    CreateObject("Wscript.Shell").Run RunCmd, 0 , True
-End If
-        """,
-        )
 
         self.create_batch_script(
             "WinPythonIni.py",  # Replaces winpython.vbs, and a bit of env.bat
