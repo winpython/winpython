2181c4fec7d6ecde49b097bba52556f1f91bcdf0
stonebig <stonebig34@gmail.com>
2025-04-12 12:12:26 +0200

    update version as tweaks/fix recently

--- winpython/__init__.py
+++ winpython/__init__.py
@@ -28,6 +28,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '15.1.20250405'
+__version__ = '15.2.20250412'
 __license__ = __doc__
 __project_url__ = 'http://winpython.github.io/'
