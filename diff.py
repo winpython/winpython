04aebb4dc04e9862e2014c5adb53f3832c5fd4eb
stonebig <stonebig34@gmail.com>
2025-04-14 21:01:21 +0200

    simplify diff.py using some copilote AI suggestions

--- diff.py
+++ diff.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 #
+# WinPython diff.py script
 # Copyright © 2013 Pierre Raybaut
+# Copyright © 2014-2025+ The Winpython development team https://github.com/winpython/
 # Licensed under the terms of the MIT License
 # (see winpython/__init__.py for details)
 
-"""
-WinPython diff script
-
-Created on Tue Jan 29 11:56:54 2013
-"""
 
 import os
 from pathlib import Path
 import re
 import shutil
-
-# Local imports
 from winpython import utils
 
-
-CHANGELOGS_DIR = str(Path(__file__).parent / "changelogs")
-assert Path(CHANGELOGS_DIR).is_dir()
+CHANGELOGS_DIR = Path(__file__).parent / "changelogs"
+assert CHANGELOGS_DIR.is_dir()
 
 
 class Package(object):
@@ -54,10 +48,7 @@ class Package(object):
         return f"  * [{self.name}]({self.url}) {self.version} ({self.description})\r\n"
 
     def upgrade_wiki(self, other):
-        # wheel replace '-' per '_' in package name
-        assert (
-            self.name.replace("-", "_").lower() == other.name.replace("-", "_").lower()
-        )
+        assert self.name.replace("-", "_").lower() == other.name.replace("-", "_").lower()
         return f"  * [{self.name}]({self.url}) {other.version} → {self.version} ({self.description})\r\n"
 
 
@@ -68,48 +59,34 @@ class PackageIndex(object):
     HEADER_LINE1 = "Name | Version | Description"
     HEADER_LINE2 = "-----|---------|------------"
 
-    def __init__(
-        self,
-        version,
-        basedir=None,
-        flavor="",
-        architecture=64,
-    ):
+    def __init__(self, version, basedir=None, flavor="", architecture=64):
         self.version = version
-        self.other_packages = {}
-        self.python_packages = {}
         self.flavor = flavor
         self.basedir = basedir
         self.architecture = architecture
+        self.other_packages = {}
+        self.python_packages = {}
         self.from_file(basedir)
 
     def from_file(self, basedir):
-        fname = str(
-            Path(CHANGELOGS_DIR)
-            / f"WinPython{self.flavor}-{self.architecture}bit-{self.version}.md"
-        )
-
-        try:
-            with open(fname, "r", encoding = 'utf-8') as fdesc:  # python3 doesn't like 'rb'
-                text = fdesc.read()
-        except:
-            with open(fname, "r") as fdesc:  # python3 doesn't like 'rb'
-                text = fdesc.read()            
+        fname = CHANGELOGS_DIR / f"WinPython{self.flavor}-{self.architecture}bit-{self.version}.md"
+        if not fname.exists():
+            raise FileNotFoundError(f"Changelog file not found: {fname}")
+        with open(fname, "r", encoding = 'utf-8') as fdesc:
+            text = fdesc.read()
         self.from_text(text)
 
     def from_text(self, text):
         version = re.match(self.WINPYTHON_PATTERN + self.flavor, text).groups()[0]
         assert version == self.version
-        tools_flag = False
-        python_flag = False
+        tools_flag = python_flag = False
         for line in text.splitlines():
             if line:
                 if line == self.TOOLS_LINE:
-                    tools_flag = True
+                    tools_flag, python_flag = True, False
                     continue
                 elif line == self.PYTHON_PACKAGES_LINE:
-                    tools_flag = False
-                    python_flag = True
+                    tools_flag, python_flag = False, True
                     continue
                 elif line in (
                     self.HEADER_LINE1,
@@ -127,42 +104,24 @@ class PackageIndex(object):
                         self.python_packages[package.name] = package
 
 
-def diff_package_dicts(dict1_in, dict2_in):
-    """Return difference between package dict1 and package dict2"""
-    text = ""
+def diff_package_dicts(old_packages, new_packages):
+    """Return difference between package old and package new"""
+
     # wheel replace '-' per '_' in key
-    dict1 = {}
-    dict2 = {}
-    for key in dict1_in:
-        dict1[key.replace("-", "_").lower()] = dict1_in[key]
-    for key in dict2_in:
-        dict2[key.replace("-", "_").lower()] = dict2_in[key]
-    set1, set2 = set(dict1.keys()), set(dict2.keys())
+    old = {k.replace("-", "_").lower(): v for k, v in old_packages.items()}
+    new = {k.replace("-", "_").lower(): v for k, v in new_packages.items()}
+    text = ""
     # New packages
-    new = sorted(set2 - set1)
-    if new:
-        text += "New packages:\r\n\r\n"
-        for name in new:
-            package = dict2[name]
-            text += package.to_wiki()
-        text += "\r\n"
+    if new_keys := sorted(set(new) - set(old)):
+       text += "New packages:\r\n\r\n" + "".join(new[k].to_wiki() for k in new_keys) + "\r\n"
+
     # Upgraded packages
-    upgraded_list = []
-    for name in sorted(set1 & set2):
-        package1 = dict1[name]
-        package2 = dict2[name]
-        if package1.version != package2.version:
-            upgraded_list.append(package2.upgrade_wiki(package1))
-    if upgraded_list:
-        text += "Upgraded packages:\r\n\r\n" + f"{''.join(upgraded_list)}" + "\r\n"
+    if upgraded := [new[k].upgrade_wiki(old[k]) for k in sorted(set(old) & set(new)) if old[k].version != new[k].version]:
+        text += "Upgraded packages:\r\n\r\n" + f"{''.join(upgraded)}" + "\r\n"
+
     # Removed packages
-    removed = sorted(set1 - set2)
-    if removed:
-        text += "Removed packages:\r\n\r\n"
-        for name in removed:
-            package = dict1[name]
-            text += package.to_wiki()
-        text += "\r\n"
+    if removed_keys := sorted(set(old) - set(new)):
+        text += "Removed packages:\r\n\r\n" + "".join(old[k].to_wiki() for k in removed_keys) + "\r\n"
     return text
 
 
@@ -190,23 +149,13 @@ def find_closer_version(version1, basedir=None, flavor="", architecture=64):
     else:
         return version_below 
 
-def compare_package_indexes(
-    version2,
-    version1=None,
-    basedir=None,
-    flavor="",
-    flavor1=None,
-    architecture=64,
-):
+def compare_package_indexes(version2, version1=None, basedir=None, flavor="", flavor1=None,architecture=64):
     """Compare two package index Wiki pages"""
-    if version1 is None:
-        version1 = find_closer_version(
-            version2,
-            basedir=basedir,
-            flavor=flavor,
-            architecture=architecture,
-        )
-    flavor1 = flavor1 if flavor1 is not None else flavor
+    version1 = version1 if version1 else find_closer_version(version2, basedir, flavor, architecture)
+    flavor1 = flavor1 if flavor1 else flavor
+    pi1 = PackageIndex(version1, basedir, flavor1, architecture)
+    pi2 = PackageIndex(version2, basedir, flavor, architecture)
+    
     text = "\r\n".join(
         [
             f"## History of changes for WinPython-{architecture}bit {version2 + flavor}",
@@ -218,18 +167,7 @@ def compare_package_indexes(
             "",
         ]
     )
-    pi1 = PackageIndex(
-        version1,
-        basedir=basedir,
-        flavor=flavor1,
-        architecture=architecture,
-    )
-    pi2 = PackageIndex(
-        version2,
-        basedir=basedir,
-        flavor=flavor,
-        architecture=architecture,
-    )
+
     tools_text = diff_package_dicts(pi1.other_packages, pi2.other_packages)
     if tools_text:
         text += PackageIndex.TOOLS_LINE + "\r\n\r\n" + tools_text
@@ -248,101 +186,22 @@ def _copy_all_changelogs(version, basedir, flavor="", architecture=64):
             % (flavor, architecture, basever),
             name,
         ):
-            shutil.copyfile(
-                str(Path(CHANGELOGS_DIR) / name),
-                str(Path(basedir) / f"bu{flavor}" / name),
-            )
+            shutil.copyfile(CHANGELOGS_DIR / name, Path(basedir) / f"bu{flavor}" / name)
 
 
-def write_changelog(
-    version2,
-    version1=None,
-    basedir=None,
-    flavor="",
-    architecture=64,
-):
+def write_changelog(version2, version1=None, basedir=None, flavor="", architecture=64):
     """Write changelog between version1 and version2 of WinPython"""
-    _copy_all_changelogs(
-        version2,
-        basedir,
-        flavor=flavor,
-        architecture=architecture,
-    )
-    print(
-        "comparing_package_indexes",
-        version2,
-        basedir,
-        flavor,
-        architecture,
-    )
-    text = compare_package_indexes(
-        version2,
-        version1,
-        basedir=basedir,
-        flavor=flavor,
-        architecture=architecture,
-    )
-    fname = str(
-        Path(basedir)
-        / f"bu{flavor}"
-        / f"WinPython{flavor}-{architecture}bit-{version2}_History.md"
-    )
+    _copy_all_changelogs(version2, basedir, flavor, architecture)
+    print("comparing_package_indexes", version2, basedir, flavor, architecture)
+    changelog_text = compare_package_indexes(version2, version1, basedir, flavor, architecture=architecture)
+    output_file = Path(basedir) / f"bu{flavor}" / f"WinPython{flavor}-{architecture}bit-{version2}_History.md"
 
-    with open(fname, "w", encoding="utf-8-sig") as fdesc:  # python 3 need
-        fdesc.write(text)
+    with open(output_file, "w", encoding="utf-8-sig") as fdesc:
+        fdesc.write(changelog_text)
     # Copy to winpython/changelogs
-    shutil.copyfile(fname, str(Path(CHANGELOGS_DIR) / Path(fname).name))
-
-
-def test_parse_package_index_wiki(version, basedir=None, flavor="", architecture=64):
-    """Parse the package index Wiki page"""
-    pi = PackageIndex(
-        version,
-        basedir=basedir,
-        flavor=flavor,
-        architecture=architecture,
-    )
-    utils.print_box(f"WinPython {pi.version}:")
-    utils.print_box("Tools:")
-    for package in pi.other_packages.values():
-        print(package)
-        print("")
-    utils.print_box("Python packages:")
-    for package in pi.python_packages.values():
-        print(package)
-        print("")
-
-
-def test_compare(basedir, version2, version1, architecture=64):
-    print(
-        compare_package_indexes(
-            basedir,
-            version2,
-            version1,
-            architecture=architecture,
-        )
-    )
+    shutil.copyfile(output_file, CHANGELOGS_DIR / output_file.name)
 
 
 if __name__ == "__main__":
-    print(
-        compare_package_indexes(
-            version2="3.7.4.0",
-            version1="3.7.2.0",
-            basedir=r"C:\WinP\bd37",
-            flavor="Zero",
-            flavor1="Zero",
-            architecture=32,
-        )
-    )
-    write_changelog(
-        version2="3.7.4.0",
-        version1="3.7.2.0",
-        basedir=r"C:\WinP\bd37",
-        flavor="Ps2",
-        architecture=64,
-    )
-    # test_parse_package_index_wiki('2.7.3.3')
-    # print(compare_package_indexes('2.7.3.3', '2.7.3.1'))
-    # write_changelog('2.7.4.1', '2.7.4.0')
-    # write_changelog('3.3.0.0beta2', '3.3.0.0beta1')
+    print(compare_package_indexes("3.7.4.0", "3.7.2.0", "C:\WinP\bd37", "Zero", architecture=32))
+    write_changelog("3.7.4.0", "3.7.2.0", r"C:\WinP\bd37", "Ps2", architecture=64)
