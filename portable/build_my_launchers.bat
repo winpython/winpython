a97b731be1e44a58fb1a6c6b4085cff0c8bf7a02
stonebig <stonebig34@gmail.com>
2025-05-08 19:46:20 +0200

    pushing on a proposed directory future launchers

--- portable/build_my_launchers.bat
+++ portable/build_my_launchers.bat
@@ -10,23 +10,23 @@ set do_launcher=%~dp0launchers_src\build_one_launcher.bat
 set do_launcher_original=%~dp0launchers_src_original\build_one_launcher.bat
 
 ::WINDOWS launchers
-call %do_launcher%  "python.ico" "winidle.bat" "IDLE (Python GUI)" WINDOWS
+rem call %do_launcher%  "python.ico" "winidle.bat" "IDLE (Python GUI)" WINDOWS proposed
 
 echo displace this pause if you want to re-build more
 pause
-exit
+rem exit
 
 
-call %do_launcher% "powershell.ico" "cmd_ps.bat" "WinPython Powershell Prompt" WINDOWS
-call %do_launcher%  "spyder.ico" "winspyder.bat" "Spyder" WINDOWS
-call %do_launcher%  "spyder_reset.ico" "spyder_reset.bat" "Spyder reset" WINDOWS
-call %do_launcher%  "code.ico" "winvscode.bat" "VS Code" WINDOWS
+call %do_launcher% "powershell.ico" "cmd_ps.bat" "WinPython Powershell Prompt" WINDOWS proposed
+call %do_launcher%  "spyder.ico" "winspyder.bat" "Spyder" WINDOWS proposed
+call %do_launcher%  "spyder_reset.ico" "spyder_reset.bat" "Spyder reset" WINDOWS proposed
+call %do_launcher%  "code.ico" "winvscode.bat" "VS Code" WINDOWS proposed
 
 :: CONSOLE launchers
-call %do_launcher%  "cmd.ico" "cmd.bat" "WinPython Command Prompt" CONSOLE
-call %do_launcher%  "python.ico" "winpython.bat" "WinPython Interpreter" CONSOLE
-call %do_launcher%  "jupyter.ico" "winipython_notebook.bat" "Jupyter Notebook" CONSOLE
-call %do_launcher%  "jupyter.ico" "winjupyter_lab.bat" "Jupyter Lab" CONSOLE
-call %do_launcher%  "winpython.ico" "wpcp.bat" "WinPython Control Panel" CONSOLE
+call %do_launcher%  "cmd.ico" "cmd.bat" "WinPython Command Prompt" CONSOLE proposed
+call %do_launcher%  "python.ico" "winpython.bat" "WinPython Interpreter" CONSOLE proposed
+call %do_launcher%  "jupyter.ico" "winipython_notebook.bat" "Jupyter Notebook" CONSOLE proposed
+call %do_launcher%  "jupyter.ico" "winjupyter_lab.bat" "Jupyter Lab" CONSOLE proposed
+call %do_launcher%  "winpython.ico" "wpcp.bat" "WinPython Control Panel" CONSOLE proposed
 pause
 
