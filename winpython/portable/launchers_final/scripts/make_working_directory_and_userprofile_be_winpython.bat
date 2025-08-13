35e5ced7e8360c6114644e8f6424e102e469e77b
stonebig <stonebig34@gmail.com>
2025-08-13 21:02:54 +0200

    reduce further .bat

--- winpython/portable/launchers_final/scripts/make_working_directory_and_userprofile_be_winpython.bat
+++
@@ -1,3 +0,0 @@
-call "%~dp0env_for_icons.bat"
-"%PYTHON%" -c "from wppm.utils import patch_sourcefile;patch_sourcefile(r'%~dp0..\settings\winpython.ini', '[active_environment', '[inactive_environment' )"
-"%PYTHON%" -c "from wppm.utils import patch_sourcefile;patch_sourcefile(r'%~dp0..\settings\winpython.ini', '[inactive_environment_common]', '[active_environment_common]' )" 
