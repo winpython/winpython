1cfcf678a18100b22d91d29cc36c075546ba0dc7
stonebig <stonebig34@gmail.com>
2025-12-29 11:04:01 +0100

    resolve pip.exe not being there by default
    https://github.com/winpython/winpython/issues/1925

--- winpython/portable/launchers_final/scripts/upgrade_pip.bat
+++ winpython/portable/launchers_final/scripts/upgrade_pip.bat
@@ -2,6 +2,6 @@
 call "%~dp0env.bat"
 echo this will upgrade pip with latest version, then patch it for WinPython portability ok ?
 pause
-"%WINPYDIR%\python.exe" -m pip install --upgrade pip
+"%WINPYDIR%\python.exe" -m pip install --upgrade --force-reinstall pip
 "%WINPYDIR%\python.exe" -c "from wppm import wppm;dist=wppm.Distribution(r'%WINPYDIR%');dist.patch_standard_packages('pip', to_movable=True)
 pause
